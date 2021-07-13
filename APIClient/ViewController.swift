ff82a9a061d4cb97bc44d7cbb65d400083f10afb
davidyoon <qqpo12@naver.com>
2021-07-14 08:18:22 +0900

    add splash screen for testing add simple login layout

--- APIClient/ViewController.swift
+++ APIClient/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textViewUserID: UITextField!
+    
+    @IBOutlet var textViewUserPWD: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func btnLoginAction(_ sender: UIButton) {
+        
+        if let userID = textViewUserID.text {
+            print("User ID : \(userID)")
+        }
+        
+        if let userPWD = textViewUserPWD.text {
+            print("User PWD : \(userPWD)")
+        }
+        
+        
+        
 
+        
+    }
+    
 }
 
